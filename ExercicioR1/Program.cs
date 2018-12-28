d725ed067640a2dc4e366d199942d728a72f2967
caiovicto546 <46228399+caiovicto546@users.noreply.github.com>
2018-12-28 17:44:54 -0200

    Add files via upload

---
+++ ExercicioR1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioR1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
